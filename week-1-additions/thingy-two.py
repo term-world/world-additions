0f4344d1058a8a24a9c38e29b10ec155eeb1a06f
jnormile <Ynormile01@allegheny.edu>
2022-06-29 18:06:50 +0000

    Edit to keypad activity for consistency across 'thingies'

--- week-1-additions/thingy-two.py
+++ week-1-additions/thingy-two.py
@@ -2,8 +2,11 @@ def main():
     # Set the variable birth_day equal to the date of the user's birth
     birth_day = input("What is the *day* (1-31) you were born? ")
 
-    # Convert the birth_day variable into an integer, and store the result in the variable running_number
-    running_number = int(birth_day)
+    # Convert the birth_day variable into an integer, and store the result in the variable birth_day_number
+    birth_day_number = int(birth_day)
+
+    # TO-DO: STORE THE VALUE OF birth_day_number IN THE VARIABLE running_number
+
 
     # TO-DO: ADD ONE TO THE running_number VARIABLE
 
@@ -23,7 +26,7 @@ def main():
     # TO-DO: STORE THE VALUE OF THE running_number VARIABLE INTO THE VARIABLE third_digit
 
 
-    # TO-DO: INCREASE running_number BY THE VALUE OF THE VARIABLE third_digit TIMES TWO
+    # TO-DO: DOUBLE THE running_number VARIABLE
 
 
     # TO-DO: PERFORM THE OPERATION running_number DIVIDED BY 5 AND STORE THE REMAINDER OF THE OPERATION IN THE VARIABLE fourth_digit
