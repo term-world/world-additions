bb1ff7007bcb775ae48699bece66c2a9aaad2169
dluman <dluman@allegheny.edu>
2022-07-30 17:52:53 +0000

    Making Ink 'gettable' on exec

--- week-0-additions/Ink.py
+++ week-0-additions/Ink.py
@@ -1,6 +1,16 @@
+import sys
+
+from inventory import Acquire
 from inventory.Item import ItemSpec
 
 class Ink(ItemSpec):
 
   def __init__(self):
     super().__init__()
+
+def main():
+  filename = sys.argv[0]
+  Acquire(filename)
+
+if __name__ == "__main__":
+  main()
