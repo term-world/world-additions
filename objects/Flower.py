8258866391641be1f122d1c70e6c3745e7ac69e9
Douglas Luman <dluman@allegheny.edu>
2022-09-11 22:10:30 -0400

    Create Flower.py

---
+++ objects/Flower.py
@@ -0,0 +1,35 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageColor
+
+from inventory.Item import ItemSpec
+
+class Flower(ItemSpec):
+
+  def __init__(self, color: str = "red"):
+    super().__init__()
+    self.size = 100
+    self.im = Image.new(
+      'RGBA',
+      (self.size, self.size),
+      (0,0,0,0)
+    )
+    self.color = ImageColor.getrgb(color)
+
+  def __str__(self) -> str:
+    return f"It's a {self.color} flower"
+
+  def use(self) -> str:
+    draw = ImageDraw.Draw(self.im)
+    draw.rectangle(
+      ((self.size//3,0),
+      (self.size*2/3,self.size)),
+      fill = self.color
+    )
+    draw.rectangle(
+      ((0,self.size//3),
+      (self.size,self.size*2/3)),
+      fill = self.color
+    )
+    self.im.save("flower.png")
+    return "flower.png"
